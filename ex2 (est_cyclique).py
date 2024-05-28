fade1b50ac3a1249af8d36ac3d26d1d96894dbc0
FwaGras <124463406+FwaGras@users.noreply.github.com>
2024-05-28 23:05:02 +0200

    Create ex2 (est_cyclique).py

---
+++ ex2 (est_cyclique).py
@@ -0,0 +1,20 @@
+def est_cyclique(plan):
+    '''Prend en paramètre un dictionnaire `plan` correspondant à 
+    un plan d'envoi de messages (ici entre les personnes A, B, C,
+    D, E, F).
+    Renvoie True si le plan d'envoi de messages est cyclique et 
+    False sinon.'''
+    expediteur = 'A'
+    destinataire = plan[expediteur] 
+    nb_destinataires = 1
+
+    while destinataire != expediteur:
+        destinataire = plan[destinataire] 
+        nb_destinataires = nb_destinataires + 1 
+
+    return nb_destinataires == len(plan) 
+
+plan_a = {'A':'E', 'B':'F', 'C':'D', 'D':'C', 'E':'B', 'F':'A'}
+plan_b = {'A':'C', 'B':'F', 'C':'E', 'D':'A', 'E':'B', 'F':'D'}
+print(est_cyclique(plan_a))
+print(est_cyclique(plan_b))
