f618dfc5282b971f6cba98b68d52414a54adc3fc
FwaGras <124463406+FwaGras@users.noreply.github.com>
2024-06-01 23:02:39 +0200

    Create ordre_gènes.py

---
+++ ordre_gènes.py
@@ -0,0 +1,38 @@
+def est_un_ordre(tab):
+    '''
+    Renvoie True si tab est de longueur n et contient tous les
+    entiers de 1 à n, False sinon
+    '''
+    n = len(tab)
+    # les entiers vus lors du parcours
+    vus = [] 
+
+    for x in tab:
+        if x < 1 or x > n or x in vus: 
+            return False
+        vus.append(x) 
+    return True
+
+def nombre_points_rupture(ordre):
+    '''
+    Renvoie le nombre de point de rupture de ordre qui représente 
+    un ordre de gènes de chromosome
+    '''
+    # on vérifie que ordre est un ordre de gènes
+    assert est_un_ordre(ordre) == True, "ordre n'est pas un ordre de gènes" 
+    n = len(ordre)
+    nb = 0
+    if ordre[0] != 1: # le premier n'est pas 1 
+        nb = nb + 1
+    i = 0
+    while i < n - 1: 
+        if ordre[i+1] - ordre[i] not in [-1, 1]: # l'écart n'est pas 1 
+            nb = nb + 1
+        i = i + 1
+    if ordre[i] != n: # le dernier n'est pas n 
+        nb = nb + 1
+    return nb
+
+assert est_un_ordre([1, 6, 2, 8, 3, 7]) == False, "erreur"
+assert est_un_ordre([5, 4, 3, 6, 7, 2, 1, 8, 9]) == True , "erreur"
+assert nombre_points_rupture([5, 4, 3, 6, 7, 2, 1, 8, 9]) == 4, "erreur"
